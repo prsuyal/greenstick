533acddb9ff3aa405a9397421b72add04d561a40
pranshu suyal <suyalpranshu@gmail.com>
2024-05-04 22:40:52 -0400

    landing page work

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -1,13 +1,36 @@
 import React, { useEffect, useRef, useState } from "react";
 import { gsap } from "gsap";
 import { useNavigate } from "react-router-dom";
+import gsLogoBlack from "../assets/images/logo-black.svg";
+import preview from "../assets/images/dash-left-only.svg";
+import Typed from "typed.js";
+import emailIcon from "../assets/images/email-icon.svg";
+import "../assets/styles/customStyles.css";
 
 const LandingPage = () => {
   const svgRef = useRef(null);
+  const contentRef = useRef(null);
   const navigate = useNavigate();
   const [animationComplete, setAnimationComplete] = useState(false);
+  const el = useRef(null);
+  useEffect(() => {
+    const options = {
+      strings: ["trading", "crypto", "stocks", "options", "futures"],
+      typeSpeed: 50,
+      backSpeed: 50,
+      loop: true,
+      backDelay: 2500,
+      showCursor: true,
+      cursorChar: "|",
+    };
+
+    const typed = new Typed(el.current, options);
+    return () => typed.destroy();
+  }, []);
 
   useEffect(() => {
+    gsap.set(contentRef.current, { autoAlpha: 0 });
+
     const tl = gsap.timeline({
       defaults: { ease: "power1.out" },
       onComplete: () => setAnimationComplete(true),
@@ -50,15 +73,24 @@ const LandingPage = () => {
       });
     });
     tl.to(svgRef.current, {
-        duration: 2, 
-        onComplete: () => {
-          gsap.to(svgRef.current, {
-            autoAlpha: 0,
-            duration: 1,
-            onComplete: () => navigate("/register"),
-          });
-        },
-      });
+      duration: 2,
+      onComplete: () => {
+        gsap.to(svgRef.current, {
+          autoAlpha: 0,
+          duration: 1,
+          onComplete: () => {
+            setAnimationComplete(true);
+            gsap.fromTo(
+              contentRef.current,
+              { autoAlpha: 0 },
+              { autoAlpha: 1, duration: 1 }
+            );
+          },
+        });
+      },
+    });
+
+    return () => tl.kill();
   }, [navigate]);
   return (
     <>
@@ -129,6 +161,72 @@ const LandingPage = () => {
             />
           </g>
         </svg>{" "}
+      </div>
+      <div ref={contentRef} className="absolute top-0 left-0 w-full h-screen">
+        <div className="fixed top-0 left-0 w-full z-10 bg-white shadow-sm">
+          <div className="max-w-7xl mx-auto px-6 py-4 flex justify-between items-center">
+            <img src={gsLogoBlack} alt="Greenstick logo" className="h-14" />
+            <button
+              className="border-2 border-black text-black font-bold bg-white py-2 px-4 rounded-md font-[Poppins] hover:text-white hover:bg-black transition-colors duration-300"
+              onClick={() => navigate("/login")}
+            >
+              Log in
+            </button>
+          </div>
+        </div>
+        <div className="pt-24 flex flex-col lg:flex-row justify-between items-center min-h-screen">
+          <div className="max-w-7xl mx-auto px-6 w-full">
+            <div className="flex flex-col lg:flex-row items-center justify-between">
+              <div>
+                <h1 className="text-4xl sm:text-5xl font-[Poppins] font-bold">
+                  Introducing <span className="text-gs-dark-green">Green</span>
+                  stick
+                </h1>
+                <h2 className="mt-10 text-xl sm:text-2xl font-[Poppins] font-light">
+                  Building the best way to learn{" "}
+                  <span ref={el} className="text-gs-dark-green"></span>
+                </h2>
+                <div className="relative mt-10">
+                  <input
+                    type="email"
+                    placeholder="Your email address"
+                    className="pl-12 pr-20 py-4 border-2 border-gray-300 rounded-lg w-full"
+                    style={{
+                      backgroundImage: `url(${emailIcon})`,
+                      backgroundPosition: "12px center",
+                      backgroundSize: "20px 20px",
+                      backgroundRepeat: "no-repeat",
+                    }}
+                  />
+                  <button
+                    onClick={() => navigate("/register")}
+                    className="absolute right-4 top-1/2 transform -translate-y-1/2 bg-black border-2 border-black text-white font-bold py-2 px-4 rounded-md hover:text-black hover:bg-white transition-colors duration-300"
+                  >
+                    Register
+                  </button>
+                </div>
+                <p className="text-sm mt-2">
+                  Or,{" "}
+                  <a
+                    href="#"
+                    className="text-black font-bold hover:underline"
+                    onClick={() => navigate("/register")}
+                  >
+                    click here
+                  </a>{" "}
+                  to sign up with Google or Apple.
+                </p>
+              </div>
+              <div>
+                <img
+                  src={preview}
+                  alt="Dashboard Preview"
+                  className="max-w-full h-auto"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
